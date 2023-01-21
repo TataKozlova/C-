bdd5cfeaccef9bbf49c7091ca33ec2727f551d24
Tatiana Kozlova <soenam@mail.ru>
2023-01-21 19:14:00 +0300

    Lesson2 Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//получить у пользователя число
+System.Console.WriteLine("Введите любое число");
+string Number = Console.ReadLine();
+//преобразовать строку в массив символов
+char[] Numbers = Number.ToCharArray();
+if (Numbers.Length >= 3){
+  System.Console.WriteLine($"третье число - {Numbers[2]}");  
+} else {
+    System.Console.WriteLine("Третьего числа нет");
+}
