d90ea4d0269539b88062b86da6a0cc50ab32f0d1
Tatiana Kozlova <soenam@mail.ru>
2023-01-21 18:50:59 +0300

    Lesson2 Task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿//получить у пользователя трехзначное число
+System.Console.WriteLine("Введите трехзначное число");
+string Number = Console.ReadLine();
+//преобразовать строку в массив символов
+char[] Numbers = Number.ToCharArray();
+//вывести элемент массива с индексом 1
+
+System.Console.WriteLine($"второе число - {Numbers[1]}");
