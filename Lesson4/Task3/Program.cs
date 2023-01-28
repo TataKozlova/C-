3bcd6788e003a0949928381b547b06bfde4bf8cf
Tatiana Kozlova <soenam@mail.ru>
2023-01-28 20:27:12 +0300

    Lesson4Task3

--- Lesson4/Task3/Program.cs
+++ Lesson4/Task3/Program.cs
@@ -3,3 +3,33 @@
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 //6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArray()
+{
+    int[] Array = new int[8];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(1, 100);
+    }
+    return Array;
+}
+
+void PrintArray(int[] Array)
+{
+    string Result = "[";
+    string Separator = ", ";
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (i == Array.Length - 1)
+        {
+            Separator = "]";
+        }
+        Result = Result + Array[i] + Separator;
+    }
+    Console.WriteLine(Result);
+}
+
+int[] Array = CreateArray();
+
+PrintArray(Array);
