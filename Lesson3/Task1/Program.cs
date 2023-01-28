d4c4413ae8177f5f6bf61f9ed3e589246329e1d3
Tatiana Kozlova <soenam@mail.ru>
2023-01-28 09:07:56 +0300

    Lesson3Task2

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -5,20 +5,21 @@
 //12821 -> да
 
 //23432 -> да
-int[] GetNumberArray (int Number) {
+int[] GetNumberArray(int Number)
+{
     int[] Array = new int[5];
-    int First = Number/10000;
-    int Second = (Number - First*10000)/1000;
-    int Third = (Number - First*10000 - Second*1000)/100;
-    int Fourth = (Number - First*10000 - Second*1000 - Third*100)/10;
-    int Fifth = Number - First*10000 - Second*1000 - Third*100 - Fourth*10;
+    int First = Number / 10000;
+    int Second = (Number - First * 10000) / 1000;
+    int Third = (Number - First * 10000 - Second * 1000) / 100;
+    int Fourth = (Number - First * 10000 - Second * 1000 - Third * 100) / 10;
+    int Fifth = Number - First * 10000 - Second * 1000 - Third * 100 - Fourth * 10;
     Array[0] = First;
     Array[1] = Second;
     Array[2] = Third;
     Array[3] = Fourth;
     Array[4] = Fifth;
     return Array;
-    
+
 };
 //Получить у пользователя пятизначное число
 Console.WriteLine("Введите пятизначное число");
@@ -30,6 +31,6 @@ int[] Array = GetNumberArray(Number);
 string IsPalindrom = "нет, это число не палиндром";
 if (Array[0] == Array[4] && Array[1] == Array[3])
 {
- IsPalindrom = "да, это число палиндром";
+    IsPalindrom = "да, это число палиндром";
 }
 Console.WriteLine($"{Number} -> {IsPalindrom}");
--- Lesson3/Task2/Program.cs
+++ Lesson3/Task2/Program.cs
@@ -8,25 +8,25 @@ int[] GetCoord(string StringPoint)
 {
     string[] ArrayCoordString = StringPoint.Split(new char[] { ',' });
     int[] ArrayCoordInt = new int[3];
-    for(int i = 0; i < ArrayCoordString.Length; i++)
+    for (int i = 0; i < ArrayCoordString.Length; i++)
     {
         int Number = Convert.ToInt32(ArrayCoordString[i]);
         ArrayCoordInt[i] = Number;
     }
     return ArrayCoordInt;
 }
-Console.WriteLine ("Введите координаты первой точки через запятую");
+Console.WriteLine("Введите координаты первой точки через запятую");
 string Point1 = Console.ReadLine();
 int[] Coord1 = GetCoord(Point1);
 //Запросить у пользователя координаты втрой точки
-Console.WriteLine ("Введите координаты второй точки через запятую");
+Console.WriteLine("Введите координаты второй точки через запятую");
 string Point2 = Console.ReadLine();
 int[] Coord2 = GetCoord(Point2);
 double Pow1 = Math.Pow((Coord1[0] - Coord2[0]), 2);
 double Pow2 = Math.Pow((Coord1[1] - Coord2[1]), 2);
 double Pow3 = Math.Pow((Coord1[2] - Coord2[2]), 2);
 double Result = Math.Sqrt(Pow1 + Pow2 + Pow3);
-double RoundResult = Math.Round(Result, 3);
-Console.WriteLine($"Расстояние между двумя точками A({Coord1[0]}, {Coord1[1]}, {Coord1[2]} и B({Coord2[0]}, {Coord2[1]}, {Coord2[2]}) будет {RoundResult}");
+double RoundResult = Math.Round(Result, 2);
+Console.WriteLine($"Расстояние между двумя точками A({Coord1[0]}, {Coord1[1]}, {Coord1[2]}) и B({Coord2[0]}, {Coord2[1]}, {Coord2[2]}) будет {RoundResult}");
 
 //
