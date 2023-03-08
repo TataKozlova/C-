df163023ba16d46302a256aae0c9a86ac60753cb
Tatiana Kozlova <soenam@mail.ru>
2023-03-08 20:31:33 +0300

    add ArrayToString

---
+++ Final Work/Program.cs
@@ -0,0 +1,23 @@
+﻿//функция, которая возвращает строку из полученного массива
+
+string ArrayToString(string[] Words)
+{
+    string Result = "[";
+    string Separator = ", ";
+
+    for (int i = 0; i < Words.Length; i++)
+    {
+        if (i == Words.Length - 1)
+        {
+            Separator = "]";
+        }
+        Result = Result + Words[i] + Separator;
+    }
+    return Result;
+}
+
+string[] words = {"кот", "ток", "торт"};
+
+string test = ArrayToString(words);
+
+Console.WriteLine(test);
